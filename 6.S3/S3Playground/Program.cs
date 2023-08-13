0d31c7a3a9f74dc629053c916625f72a0a2e5309
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-08-13 17:28:13 +0600

    Downloading and processing data using C#

--- 6.S3/S3Playground/Program.cs
+++ 6.S3/S3Playground/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using System.Text;
 
 
-var s3Client = new AmazonS3Client();
 
 // image upload--------------------------------------------
 
+//var s3Client = new AmazonS3Client();
 //using var inputStream = new FileStream("D:\\Work\\Personal\\AWS\\AwsFundamentals\\6.S3\\S3Playground\\movies.jpg", FileMode.Open, FileAccess.Read);
 
 //var putObjectRequest = new PutObjectRequest
@@ -18,16 +19,38 @@ var s3Client = new AmazonS3Client();
 
 //await s3Client.PutObjectAsync(putObjectRequest);
 
-//file upload -----------------------
 
-using var inputStream = new FileStream("D:\\Work\\Personal\\AWS\\AwsFundamentals\\6.S3\\S3Playground\\movies.csv", FileMode.Open, FileAccess.Read);
+//file upload --------------------------------------------------------------------
 
-var putObjectRequest = new PutObjectRequest
+//var s3Client = new AmazonS3Client();
+//using var inputStream = new FileStream("D:\\Work\\Personal\\AWS\\AwsFundamentals\\6.S3\\S3Playground\\movies.csv", FileMode.Open, FileAccess.Read);
+
+//var putObjectRequest = new PutObjectRequest
+//{
+//    BucketName = "awsfundamenetalss3",
+//    Key = "files/movies.csv",
+//    ContentType = "text/csv",
+//    InputStream = inputStream
+//};
+
+//await s3Client.PutObjectAsync(putObjectRequest);
+
+
+// read file from s3 ----------------------------------------------------
+
+var s3Client = new AmazonS3Client();
+
+var getObjectRquest = new GetObjectRequest()
 {
     BucketName = "awsfundamenetalss3",
     Key = "files/movies.csv",
-    ContentType = "text/csv",
-    InputStream = inputStream
 };
 
-await s3Client.PutObjectAsync(putObjectRequest);
+var response  = await s3Client.GetObjectAsync(getObjectRquest);
+
+using var memoryStream = new MemoryStream();
+response.ResponseStream.CopyTo(memoryStream);
+
+var text  = Encoding.Default.GetString(memoryStream.ToArray());
+
+Console.WriteLine(text);
